b7fc276a4663736ebb3699a874e13ea07495259d
raphaxl <rapjoker@outlook.com>
2018-06-21 10:58:53 +0200

    Add files via upload
    Problem: Regeln stimmen nicht

--- text_nachbarn.java
+++ text_nachbarn.java
@@ -1,6 +1,8 @@
 package package1;
 
 public class text_nachbarn {
+	
+	// Zahl im Variable entfernen um zu benutzen
 
 	static int[][] spielfeld = {
 			{0, 0, 0},
@@ -8,22 +10,38 @@ public class text_nachbarn {
  			{0, 0, 0}
 	};
 	
-	static int[][] zweiteGen = new int[spielfeld.length][spielfeld[spielfeld.length - 1].length];
+	static int[][] spielfeld2 = {
+			{0, 1, 0},
+			{0, 1, 1},
+ 			{1, 0, 1}
+	};
+	
+	static int[][] spielfeld3 = {
+			{0, 0, 0, 0, 0},
+			{0, 0, 1, 0, 0},
+			{0, 0, 1, 1, 0},
+ 			{0, 1, 0, 1, 0},
+ 			{0, 0, 0, 0, 0}
+	};
+	
+	static int[][] zweiteGen = new int[spielfeld.length][spielfeld[spielfeld.length - 1].length]; 
 	
 	public static void main(String[] args) {
 
 		// 1 = Ein Generation, 3 = Drei Generationen
-		generationen(1);
-		neueGen();
+		generationen(3);
+		// neueGen();
 	}
 	
 	public static void generationen(int gen) {
 		for(int schritt = 0; schritt < gen; schritt++) {
-			System.out.println("Generation: " + (schritt + 1));
-			
+			// System.out.println("Generation: " + (schritt + 1));
+			neueGen();
 			// jedes Feld durchgehen
 			felderPruefen(spielfeld);
-			System.out.print("\n");
+
+			spielfeld = zweiteGen;
+			// System.out.print("\n"); 
 		}
 	}
 	
@@ -31,7 +49,7 @@ public class text_nachbarn {
 		for(int reihe = 0; reihe < feld.length; reihe++) {
 			for(int zeile = 0; zeile < feld[feld.length - 1].length; zeile++) {
 				
-				System.out.println("Zeile: " + zeile + " Reihe: " + reihe);
+				// System.out.println("Zeile: " + zeile + " Reihe: " + reihe);
 				
 				// 3x3 Feld prüfen
 				feldPruefen(reihe, zeile);
@@ -70,27 +88,27 @@ public class text_nachbarn {
 						
 						if(spielfeld[reihe][zeile] < 1) {
 							// Wenn Zelle tot dann '-'
-							System.out.print('-'); // Tote Zelle
+							// System.out.print('-'); // Tote Zelle
 							lebendig = false;
 						} else {
 							// Wenn mittlere Zelle lebendig, dann '+'
-							System.out.print('+'); // Lebende Zelle
+							// System.out.print('+'); // Lebende Zelle
 							lebendig = true;
 						}
 						
 					} else {
 						// Sonst normale Zahlen ausgeben
-						System.out.print(spielfeld[reihe + i][zeile + j]);
+						// System.out.print(spielfeld[reihe + i][zeile + j]);
 					}
 					
 				} catch (Exception ex) {
 					// Wenn kein Zahl gibt (ausserhalb von Spielfeld)
-					System.out.print('x'); // '0' ?
+					// System.out.print('x'); // '0' ?
 				}
 			}
-			System.out.print("\n");
+			// System.out.print("\n");
 		}
-		System.out.println("Nachbarn: " + nachbarAnzahl);
+		// System.out.println("Nachbarn: " + nachbarAnzahl);
 		
 		zelleStatus(lebendig, nachbarAnzahl, reihe, zeile);
 		
@@ -102,38 +120,41 @@ public class text_nachbarn {
 	public static void zelleStatus(boolean lebendig, int nachbarAnzahl, int reihe, int zeile) {
 		// Tote Zelle: 		3 Nachbarn 				> wiederbelebt
 		if((lebendig == false) && nachbarAnzahl == 3) {
-			System.out.println("Zelle wiederbelebt");
+			// System.out.println("Zelle wiederbelebt");
 			zweiteGen[reihe][zeile] = 1;
 		}
 		
 		// Lebende Zelle: 	wengier als 2 Nachbarn 	> stirbt
 		// Lebende Zelle: 	mehr als 3 Nachbarn 	> stirbt
 		if((lebendig == true) && (nachbarAnzahl < 2 || nachbarAnzahl > 3)) {
-			System.out.println("Zelle stirbt");
+			// System.out.println("Zelle stirbt");
 			zweiteGen[reihe][zeile] = 0;
 		}
 		
 		// Lebende Zelle: 	2 oder 3 Nachbarn 		> lebt weiter
 		if((lebendig == true) && (nachbarAnzahl == 2) || (nachbarAnzahl == 3)) {
-			System.out.println("Zelle lebt weiter");
+			// System.out.println("Zelle lebt weiter");
 			zweiteGen[reihe][zeile] = 1;
 		}
 		
-		// nicht nötig, nur für die Ausgabe
+/*		// nicht nötig, nur für die Ausgabe
 		if((lebendig == false) && (nachbarAnzahl < 3 || nachbarAnzahl > 3)) {
-			System.out.println("Passiert nichts");
+			// System.out.println("Passiert nichts");
 			zweiteGen[reihe][zeile] = 0; //?
+		}*/
+		else {
+			zweiteGen[reihe][zeile] = 0;
 		}
 	}
 	
 	public static void neueGen() {
 		
-		System.out.println("Zweite Generation");
-		for(int i = 0; i < zweiteGen.length; i++) {
-			for(int j = 0; j < zweiteGen[zweiteGen.length - 1].length; j++) {
-				System.out.print(zweiteGen[i][j]);
+		for(int i = 0; i < spielfeld.length; i++) {
+			for(int j = 0; j < spielfeld[spielfeld.length - 1].length; j++) {
+				System.out.print(spielfeld[i][j]);
 			}
 			System.out.print("\n");
 		}
+		System.out.println();
 	}
 }
